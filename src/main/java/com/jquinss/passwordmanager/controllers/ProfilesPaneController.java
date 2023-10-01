863bbae3b6713e94570c04c9ee2c5c8e29214d31
Joaquin <47039871+jquinss@users.noreply.github.com>
2023-10-01 07:33:07 +0100

    first commit

---
+++ src/main/java/com/jquinss/passwordmanager/controllers/ProfilesPaneController.java
@@ -0,0 +1,20 @@
+package com.jquinss.passwordmanager.controllers;
+
+import javafx.fxml.FXML;
+
+public class ProfilesPaneController {
+    @FXML
+    private void addProfile() {
+        // ToDo
+    }
+
+    @FXML
+    private void loadProfile() {
+        // ToDo
+    }
+
+    @FXML
+    private void removeProfile() {
+        // ToDo
+    }
+}
