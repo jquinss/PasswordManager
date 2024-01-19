26455e5f5d47cbcfd35271bc92aa052dca3a87a4
Joaquin <47039871+jquinss@users.noreply.github.com>
2024-01-19 13:01:28 +0000

    added PasswordManagerController

--- src/main/java/com/jquinss/passwordmanager/app/Main.java
+++ src/main/java/com/jquinss/passwordmanager/app/Main.java
@@ -1,26 +1,15 @@
 package com.jquinss.passwordmanager.app;
 
-import com.jquinss.passwordmanager.controllers.ProfilesPaneController;
+import com.jquinss.passwordmanager.controllers.PasswordManagerController;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Main extends Application{
     @Override
     public void start(Stage primaryStage) {
         try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/jquinss/passwordmanager/fxml/ProfilesPane.fxml"));
-            primaryStage.setTitle("Password Manager");
-            //primaryStage.getIcons().add(new Image(getClass().getResource("/com/jquinss/passwordmanager/images/logo.png").toString()));
-            AnchorPane root = (AnchorPane) fxmlLoader.load();
-            final ProfilesPaneController controller = fxmlLoader.getController();
-            //controller.setStage(primaryStage);
-            Scene scene = new Scene(root, 530, 370);
-            //scene.getStylesheets().add(getClass().getResource("/com/jquinss/passwordmanager/styles/application.css").toString());
-            primaryStage.setScene(scene);
-            primaryStage.show();
+            PasswordManagerController controller = new PasswordManagerController(primaryStage);
+            controller.loadLoginPane();
         }
         catch(Exception e) {
             e.printStackTrace();
