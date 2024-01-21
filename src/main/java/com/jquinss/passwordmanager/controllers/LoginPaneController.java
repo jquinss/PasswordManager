bf260090b930cc77431911169f4499ef91e4f029
Joaquin <47039871+jquinss@users.noreply.github.com>
2024-01-21 12:56:27 +0000

    imlemented authenticateUser() method

--- src/main/java/com/jquinss/passwordmanager/controllers/LoginPaneController.java
+++ src/main/java/com/jquinss/passwordmanager/controllers/LoginPaneController.java
@@ -1,12 +1,17 @@
 package com.jquinss.passwordmanager.controllers;
 
+import com.jquinss.passwordmanager.security.Authenticator;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
+import java.sql.SQLException;
+
 public class LoginPaneController {
+    public Label loginMessage;
     @FXML
     private TextField usernameTextField;
     @FXML
@@ -14,13 +19,41 @@ public class LoginPaneController {
     @FXML
     private Button loginButton;
 
+    private PasswordManagerController passwordManagerController;
+
+    private final Authenticator authenticator = new Authenticator();
+
     @FXML
     public void authenticateUser() {
-        System.out.println("Authenticating user");
+        String username = usernameTextField.getText();
+        String password = passwordField.getText();
+
+        try {
+            boolean validCredentials = authenticator.authenticate(username, password);
+
+            if (validCredentials) {
+                setErrorMessage("", false);
+                passwordManagerController.loadPasswordManagerPane(); }
+            else {
+                setErrorMessage("Invalid username or password", true);
+            }
+        }
+        catch (SQLException e) {
+            setErrorMessage("Error: Cannot connect to the database", true);
+        }
     }
     
     @FXML
     public void signUpUser() {
-        System.out.println("Loading registration pane");
+        passwordManagerController.loadRegistrationPane();
+    }
+
+    void setPasswordManagerController(PasswordManagerController passwordManagerController) {
+        this.passwordManagerController = passwordManagerController;
+    }
+
+    private void setErrorMessage(String text, boolean visible) {
+        loginMessage.setVisible(visible);
+        loginMessage.setText(text);
     }
 }
