a14ec1b68499ca356d53384bfb3df52a0baff8cf
Joaquin <47039871+jquinss@users.noreply.github.com>
2024-03-01 21:00:39 +0000

    created file

---
+++ src/main/java/com/jquinss/passwordmanager/controllers/TreeViewPaneController.java
@@ -0,0 +1,10 @@
+package com.jquinss.passwordmanager.controllers;
+
+import com.jquinss.passwordmanager.data.DataEntity;
+import javafx.fxml.FXML;
+import javafx.scene.control.TreeView;
+
+public class TreeViewPaneController {
+    @FXML
+    private TreeView<DataEntity> treeView;
+}
---
+++ src/main/java/com/jquinss/passwordmanager/data/DataEntity.java
@@ -0,0 +1,36 @@
+package com.jquinss.passwordmanager.data;
+
+public abstract class DataEntity {
+    private int id;
+    private String name;
+    private String description = "";
+
+    public DataEntity(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
