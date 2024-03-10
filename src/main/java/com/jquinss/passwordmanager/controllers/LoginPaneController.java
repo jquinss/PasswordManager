ede8b0ff9a12693253940aa711f2d8345665f73e
Joaquin <47039871+jquinss@users.noreply.github.com>
2024-03-10 08:25:18 +0000

    implemented methods to load the PasswordManagerPane

--- src/main/java/com/jquinss/passwordmanager/controllers/LoginPaneController.java
+++ src/main/java/com/jquinss/passwordmanager/controllers/LoginPaneController.java
@@ -1,14 +1,26 @@
 package com.jquinss.passwordmanager.controllers;
 
+import com.jquinss.passwordmanager.data.User;
+import com.jquinss.passwordmanager.exceptions.LoadKeyPairException;
+import com.jquinss.passwordmanager.managers.DatabaseManager;
+import com.jquinss.passwordmanager.managers.SettingsManager;
 import com.jquinss.passwordmanager.security.Authenticator;
+import com.jquinss.passwordmanager.util.misc.CryptoUtils;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
 import java.io.IOException;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
 import java.sql.SQLException;
+import java.util.Optional;
 
 public class LoginPaneController {
     public Label message;
@@ -16,8 +28,6 @@ public class LoginPaneController {
     private TextField usernameTextField;
     @FXML
     private PasswordField passwordField;
-    @FXML
-    private Button loginButton;
 
     private PasswordManagerController passwordManagerController;
 
@@ -34,16 +44,28 @@ public class LoginPaneController {
             if (validCredentials) {
                 hideMessage();
                 showSuccessMessage("Authentication successful");
-                passwordManagerController.loadPasswordManagerPane(); }
+                Optional<KeyPair> result = loadKeyPair(username, password);
+                if (result.isPresent()) {
+                    KeyPair keyPair = result.get();
+                    passwordManagerController.loadPasswordManagerPane(username, keyPair);
+                }
+                else {
+                    throw new LoadKeyPairException();
+                }
+            }
             else {
-                showErrorMessage("Invalid username or password");
+                showErrorMessage("Error: Invalid username or password");
                 clearFields();
             }
         }
         catch (SQLException e) {
             showErrorMessage("Error: Cannot connect to the database");
         }
-        catch (IOException e) {
+        catch (NoSuchAlgorithmException | InvalidKeySpecException | InvalidAlgorithmParameterException | NoSuchPaddingException |
+                IllegalBlockSizeException | BadPaddingException | InvalidKeyException | LoadKeyPairException e) {
+            showErrorMessage("Error: Error loading key-pair from database");
+        }
+        catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
@@ -80,5 +102,25 @@ public class LoginPaneController {
     private void clearFields() {
         usernameTextField.clear();
         passwordField.clear();
+    }
+
+    private Optional<KeyPair> loadKeyPair(String username, String password) throws SQLException, NoSuchAlgorithmException, InvalidKeySpecException,
+            InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
+        Optional<User> result = DatabaseManager.getInstance().getUserByName(username);
+        KeyPair keyPair = null;
+        if (result.isPresent()) {
+            User user = result.get();
+            byte[] publicKey = user.getPublicKey();
+            byte[] encryptedPrivateKey = user.getPrivateKey();
+
+            byte[] salt = user.getPasswordSalt();
+            IvParameterSpec ivParameterSpec = new IvParameterSpec(user.getPrivateKeyIV());
+            SecretKey key = CryptoUtils.getSecretKeyFromPassword(password, salt);
+            byte[] privateKey = CryptoUtils.decrypt(encryptedPrivateKey, SettingsManager.getInstance().getSymmetricEncryptionAlgorithm(),
+                    key, ivParameterSpec);
+
+            keyPair = CryptoUtils.loadKeyPair(publicKey, privateKey, SettingsManager.getInstance().getKeyPairAlgorithm());
+        }
+        return Optional.ofNullable(keyPair);
     }
 }
