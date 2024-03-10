bde20c00505aa6ac04185c3e37ddc46e2e27cabb
Joaquin <47039871+jquinss@users.noreply.github.com>
2024-03-10 08:35:29 +0000

    implemented constructor and methods to initiate and terminate user session

--- src/main/java/com/jquinss/passwordmanager/controllers/PasswordManagerPaneController.java
+++ src/main/java/com/jquinss/passwordmanager/controllers/PasswordManagerPaneController.java
@@ -1,9 +1,37 @@
 package com.jquinss.passwordmanager.controllers;
 
+import com.jquinss.passwordmanager.data.DataEntity;
+import com.jquinss.passwordmanager.security.UserSession;
+import com.jquinss.passwordmanager.util.misc.CryptoUtils;
+import javafx.fxml.FXML;
+import javafx.scene.control.TreeView;
+
+import java.io.IOException;
+
 public class PasswordManagerPaneController {
+    @FXML
+    public TreeView<DataEntity> treeView;
+    @FXML
+    private PasswordEntityEditorPaneController passwordEntityEditorPaneController;
+    private final UserSession userSession = new UserSession();
+    private final CryptoUtils.AsymmetricCrypto asymmetricCrypto;
     private PasswordManagerController passwordManagerController;
+
+    public PasswordManagerPaneController(String username, CryptoUtils.AsymmetricCrypto asymmetricCrypto) {
+        userSession.initiate(username);
+        this.asymmetricCrypto = asymmetricCrypto;
+    }
 
     void setPasswordManagerController(PasswordManagerController passwordManagerController) {
         this.passwordManagerController = passwordManagerController;
+    }
+
+    private void terminateUserSession() {
+        userSession.terminate();
+    }
+
+    private void logOut() throws IOException {
+        terminateUserSession();
+        passwordManagerController.loadLoginPane();
     }
 }
