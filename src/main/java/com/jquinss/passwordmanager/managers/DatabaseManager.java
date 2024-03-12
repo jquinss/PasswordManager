4312f7561879c04f0e5e8f94e3fb022f17485874
Joaquin <47039871+jquinss@users.noreply.github.com>
2024-03-12 20:47:18 +0000

    added insert sql statement to initialize default configuration

--- src/main/java/com/jquinss/passwordmanager/managers/DatabaseManager.java
+++ src/main/java/com/jquinss/passwordmanager/managers/DatabaseManager.java
@@ -1,9 +1,7 @@
 package com.jquinss.passwordmanager.managers;
 
 import com.jquinss.passwordmanager.dao.DbUserDao;
-import com.jquinss.passwordmanager.dao.DbUserProfileDao;
 import com.jquinss.passwordmanager.data.User;
-import com.jquinss.passwordmanager.data.UserProfile;
 import com.jquinss.passwordmanager.factories.DataSourceFactory;
 import org.sqlite.SQLiteDataSource;
 
@@ -11,7 +9,6 @@ import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.List;
 import java.util.Optional;
 
 public class DatabaseManager {
@@ -20,7 +17,7 @@ public class DatabaseManager {
             password BLOB NOT NULL, password_salt BLOB NOT NULL, public_key BLOB NOT NULL, private_key BLOB NOT NULL, private_key_iv BLOB NOT NULL);
             CREATE TABLE IF NOT EXISTS folder (folder_id INTEGER PRIMARY KEY, parent_folder_id INTEGER folder_name TEXT NOT NULL, description TEXT);
             CREATE TABLE IF NOT EXISTS password_entity (password_entity_id INTEGER PRIMARY KEY, name TEXT NOT NULL, 
-            username TEXT NOT NULL, password TEXT NOT NULL, URL TEXT, description TEXT, expires INTEGER NOT NULL DEFAULT 0,
+            password TEXT NOT NULL, URL TEXT, description TEXT, expires INTEGER NOT NULL DEFAULT 0,
             expiration_date TEXT, user_id INTEGER NOT NULL, folder_id INTEGER NOT NULL,
             password_policy_id INTEGER NOT NULL, FOREIGN KEY(user_id) REFERENCES user(user_id),
             FOREIGN KEY(folder_id) REFERENCES folder(folder_id)), FOREIGN KEY(password_policy_id) REFERENCES password_policy(password_policy_id);
@@ -28,6 +25,10 @@ public class DatabaseManager {
             min_length INTEGER NOT NULL, min_lower_case_chars INT NOT NULL, min_uppper_case_chars INT NOT NULL,
             min_digits INT NOT NULL, min_symbols INT NOT NULL, max_consec_equal_chars INT NOT NULL);
             """;
+
+    private static final String CREATE_INITIAL_CONFIG_STATEMENT = """
+            INSERT INTO folder VALUES (0, null, 'root', 'Root folder');
+            INSERT INTO password_policy VALUES (0, 'default', 10, 3, 3, 3, 3, 2)""";
 
     private static final String databaseURL = "jdbc:sqlite:" + SettingsManager.getInstance().getDatabasePath();
     private final DataSource dataSource = initializeDataSource();
@@ -53,6 +54,7 @@ public class DatabaseManager {
             if (conn != null) {
                 Statement stmt = conn.createStatement();
                 stmt.execute(CREATE_TABLES_STATEMENT);
+                stmt.execute(CREATE_INITIAL_CONFIG_STATEMENT);
                 System.out.println("Database has been created");
             }
         }
