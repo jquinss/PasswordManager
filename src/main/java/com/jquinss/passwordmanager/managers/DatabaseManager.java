8693c6a885b920f1f224c630568fca51f093471a
Joaquin <47039871+jquinss@users.noreply.github.com>
2024-03-17 21:49:23 +0000

    added email_address attribute to password_entity table

--- src/main/java/com/jquinss/passwordmanager/managers/DatabaseManager.java
+++ src/main/java/com/jquinss/passwordmanager/managers/DatabaseManager.java
@@ -15,14 +15,14 @@ public class DatabaseManager {
     private static final String CREATE_TABLES_STATEMENT = """
             CREATE TABLE IF NOT EXISTS user (user_id INTEGER PRIMARY KEY, user_name TEXT UNIQUE NOT NULL,
             password BLOB NOT NULL, password_salt BLOB NOT NULL, public_key BLOB NOT NULL, private_key BLOB NOT NULL, private_key_iv BLOB NOT NULL);
-            CREATE TABLE IF NOT EXISTS folder (folder_id INTEGER PRIMARY KEY, parent_folder_id INTEGER folder_name TEXT NOT NULL, description TEXT);
-            CREATE TABLE IF NOT EXISTS password_entity (password_entity_id INTEGER PRIMARY KEY, name TEXT NOT NULL, 
-            password TEXT NOT NULL, URL TEXT, description TEXT, expires INTEGER NOT NULL DEFAULT 0,
+            CREATE TABLE IF NOT EXISTS folder (folder_id INTEGER PRIMARY KEY, parent_folder_id INTEGER, folder_name TEXT NOT NULL, description TEXT);
+            CREATE TABLE IF NOT EXISTS password_entity (password_entity_id INTEGER PRIMARY KEY, name TEXT NOT NULL, user_name TEXT,
+            password TEXT NOT NULL, email_address TEXT, URL TEXT, description TEXT, expires INTEGER NOT NULL DEFAULT 0,
             expiration_date TEXT, user_id INTEGER NOT NULL, folder_id INTEGER NOT NULL,
             password_policy_id INTEGER NOT NULL, FOREIGN KEY(user_id) REFERENCES user(user_id),
             FOREIGN KEY(folder_id) REFERENCES folder(folder_id)), FOREIGN KEY(password_policy_id) REFERENCES password_policy(password_policy_id);
             CREATE TABLE IF NOT EXISTS password_policy (password_policy_id INTEGER PRIMARY KEY, password_policy_name TEXT NOT NULL,
-            min_length INTEGER NOT NULL, min_lower_case_chars INT NOT NULL, min_uppper_case_chars INT NOT NULL,
+            min_length INTEGER NOT NULL, min_lower_case_chars INT NOT NULL, min_upper_case_chars INT NOT NULL,
             min_digits INT NOT NULL, min_symbols INT NOT NULL, max_consec_equal_chars INT NOT NULL);
             """;
 
