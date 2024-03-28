639cc84f8dec94a3cdd124612d573a9496af85a0
Joaquin <47039871+jquinss@users.noreply.github.com>
2024-03-28 14:56:45 +0000

    added additional class constructor and methods

--- src/main/java/com/jquinss/passwordmanager/data/RootFolder.java
+++ src/main/java/com/jquinss/passwordmanager/data/RootFolder.java
@@ -1,7 +1,22 @@
 package com.jquinss.passwordmanager.data;
 
-public class RootFolder extends Folder{
-    public RootFolder(int id, String name) {
+public class RootFolder extends Folder {
+    private int userId;
+
+    public RootFolder(String name, int userId) {
+        super(name);
+        this.userId = userId;
+    }
+    public RootFolder(int id, String name, int userId) {
         super(id, name);
+        this.userId = userId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
     }
 }
