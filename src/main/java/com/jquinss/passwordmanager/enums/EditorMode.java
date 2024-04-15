de4ea77b24e8ff405685efe91ed6a7e5e593826f
Joaquin <47039871+jquinss@users.noreply.github.com>
2024-04-15 19:28:33 +0100

    created class

---
+++ src/main/java/com/jquinss/passwordmanager/enums/EditorMode.java
@@ -0,0 +1,18 @@
+package com.jquinss.passwordmanager.enums;
+
+import com.jquinss.passwordmanager.data.DataEntity;
+
+import java.util.Optional;
+
+public enum EditorMode {
+    CREATE, EDIT, HIDE, VIEW;
+
+    private DataEntity dataEntity;
+
+    public void setDataEntity(DataEntity dataEntity) {
+        this.dataEntity = dataEntity;
+    }
+    public Optional<DataEntity> getDataEntity() {
+        return Optional.ofNullable(dataEntity);
+    }
+}
