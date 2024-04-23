61fb86c292af4289023f33c886bf8e3135ca7450
Joaquin <47039871+jquinss@users.noreply.github.com>
2024-04-23 21:03:36 +0100

    created file

---
+++ src/main/java/com/jquinss/passwordmanager/dao/PasswordGeneratorPolicyDao.java
@@ -0,0 +1,19 @@
+package com.jquinss.passwordmanager.dao;
+
+import com.jquinss.passwordmanager.data.PasswordGeneratorPolicy;
+
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+
+public interface PasswordGeneratorPolicyDao {
+    Optional<PasswordGeneratorPolicy> getById(int id) throws SQLException;
+
+    List<PasswordGeneratorPolicy> getAll() throws SQLException;
+
+    void add(PasswordGeneratorPolicy pwdGenPolicy) throws SQLException;
+
+    void update(PasswordGeneratorPolicy pwdGenPolicy) throws SQLException;
+
+    void delete(PasswordGeneratorPolicy pwdGenPolicy) throws SQLException;
+}
