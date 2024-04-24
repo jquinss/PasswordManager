7ba7745675ac856148c52713849e8489177a44bd
Joaquin <47039871+jquinss@users.noreply.github.com>
2024-04-24 20:04:33 +0100

    modified class to implement the fluent interface

--- src/main/java/com/jquinss/passwordmanager/util/password/PasswordSpecs.java
+++ src/main/java/com/jquinss/passwordmanager/util/password/PasswordSpecs.java
@@ -6,45 +6,69 @@ public class PasswordSpecs {
     private int numDigits;
     private int numSymbols;
 
+    private PasswordSpecs(Builder builder) {
+        this.numLowerCaseChars = builder.numLowerCaseChars;
+        this.numUpperCaseChars = builder.numUpperCaseChars;
+        this.numDigits = builder.numDigits;
+        this.numSymbols = builder.numSymbols;
+    }
+
     public int getNumLowerCaseChars() {
         return numLowerCaseChars;
     }
-
-    public void setNumLowerCaseChars(int numLowerCaseChars) {
-        validatePositiveInteger(numLowerCaseChars);
-        this.numLowerCaseChars = numLowerCaseChars;
-    }
-
     public int getNumUpperCaseChars() {
         return numUpperCaseChars;
-    }
-
-    public void setNumUpperCaseChars(int numUpperCaseChars) {
-        validatePositiveInteger(numUpperCaseChars);
-        this.numUpperCaseChars = numUpperCaseChars;
     }
 
     public int getNumDigits() {
         return numDigits;
     }
 
-    public void setNumDigits(int numDigits) {
-        validatePositiveInteger(numDigits);
-        this.numDigits = numDigits;
-    }
-
     public int getNumSymbols() {
         return numSymbols;
     }
 
-    public void setNumSymbols(int numSymbols) {
-        validatePositiveInteger(numSymbols);
-        this.numSymbols = numSymbols;
-    }
+    public static class Builder {
+        private int numLowerCaseChars;
+        private int numUpperCaseChars;
+        private int numDigits;
+        private int numSymbols;
 
-    private void validatePositiveInteger(int num) {
-        if (num < 0) {
-            throw new RuntimeException("Must be a positive integer");
+        public Builder numLowerCaseChars(int numLowerCaseChars) {
+            validatePositiveInteger(numLowerCaseChars);
+            this.numLowerCaseChars = numLowerCaseChars;
+            return this;
+        }
+
+        public Builder minUppercaseChars(int numUpperCaseChars) {
+            validatePositiveInteger(numUpperCaseChars);
+            this.numUpperCaseChars = numUpperCaseChars;
+            return this;
+        }
+
+        public Builder numDigits(int numDigits) {
+            validatePositiveInteger(numDigits);
+            this.numDigits = numDigits;
+            return this;
+        }
+
+        public Builder numSymbols(int numSymbols) {
+            validatePositiveInteger(numSymbols);
+            this.numSymbols = numSymbols;
+            return this;
+        }
+
+        public PasswordSpecs build() {
+            if ((this.numLowerCaseChars + this.numUpperCaseChars + this.numDigits + this.numSymbols) <= 0) {
+                throw new RuntimeException("The password needs to have at least 1 character");
+            }
+            return new PasswordSpecs(this);
+        }
+
+        private void validatePositiveInteger(int num) {
+            if (num < 0) {
+                throw new RuntimeException("Must be a positive integer");
+            }
         }
     }
 }
