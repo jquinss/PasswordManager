ab1a12cee330b47dac42977733f9504343eb605f
Joaquin <47039871+jquinss@users.noreply.github.com>
2024-04-25 19:22:07 +0100

    implemented add, update and delete methods

--- src/main/java/com/jquinss/passwordmanager/dao/DbPasswordGeneratorPolicyDao.java
+++ src/main/java/com/jquinss/passwordmanager/dao/DbPasswordGeneratorPolicyDao.java
@@ -1,13 +1,12 @@
 package com.jquinss.passwordmanager.dao;
 
 import com.jquinss.passwordmanager.data.PasswordGeneratorPolicy;
+import com.jquinss.passwordmanager.data.PasswordPolicy;
 import com.jquinss.passwordmanager.util.password.PasswordSpecs;
+import com.jquinss.passwordmanager.util.password.PasswordStrengthCriteria;
 
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -47,17 +46,37 @@ public class DbPasswordGeneratorPolicyDao implements PasswordGeneratorPolicyDao 
 
     @Override
     public void add(PasswordGeneratorPolicy pwdGenPolicy) throws SQLException {
-        // TDDO
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement ps = buildAddPasswordGeneratorPolicyPreparedStatement(conn, pwdGenPolicy);) {
+
+            conn.setAutoCommit(false);
+            ps.executeUpdate();
+
+            Statement statement = conn.createStatement();
+
+            try (ResultSet resultSet = statement.executeQuery("SELECT last_insert_rowid()")) {
+                if (resultSet.next()) {
+                    pwdGenPolicy.setId(resultSet.getInt(1));
+                }
+                conn.commit();
+            }
+        }
     }
 
     @Override
     public void update(PasswordGeneratorPolicy pwdGenPolicy) throws SQLException {
-        // TDDO
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement ps = buildUpdatePasswordGenPolicyPreparedStatement(conn, pwdGenPolicy)) {
+            ps.executeUpdate();
+        }
     }
 
     @Override
     public void delete(PasswordGeneratorPolicy pwdGenPolicy) throws SQLException {
-        // TDDO
+        try (Connection conn = dataSource.getConnection();
+             PreparedStatement ps = buildDeletePasswordGenPolicyPreparedStatement(conn, pwdGenPolicy.getId())) {
+            ps.executeUpdate();
+        }
     }
 
     private PasswordGeneratorPolicy createPasswordGeneratorPolicy(ResultSet rs) throws SQLException {
@@ -70,6 +89,7 @@ public class DbPasswordGeneratorPolicyDao implements PasswordGeneratorPolicyDao 
 
         PasswordGeneratorPolicy pwdGenPolicy = new PasswordGeneratorPolicy(rs.getString(2), passwordSpecs);
         pwdGenPolicy.setId(rs.getInt(1));
+        pwdGenPolicy.setDefaultPolicy(rs.getBoolean(7));
 
         return pwdGenPolicy;
     }
@@ -78,6 +98,46 @@ public class DbPasswordGeneratorPolicyDao implements PasswordGeneratorPolicyDao 
         String statement = """
                 SELECT * FROM password_gen_policy WHERE password_gen_policy_id = ?""";
         PreparedStatement ps = conn.prepareStatement(statement);
+        ps.setInt(1, id);
+
+        return ps;
+    }
+
+    private PreparedStatement buildAddPasswordGeneratorPolicyPreparedStatement(Connection conn, PasswordGeneratorPolicy pwdGenPolicy) throws SQLException {
+        String statement = """
+        INSERT INTO password_gen_policy (password_gen_policy_name, lower_case_chars, upper_case_chars, digits, 
+        symbols, default_policy) VALUES (?,?,?,?,?,?)""";
+
+        return buildSetOperationPreparedStatement(conn, pwdGenPolicy, statement);
+    }
+
+    private PreparedStatement buildSetOperationPreparedStatement(Connection conn, PasswordGeneratorPolicy pwdGenPolicy,
+                                                                 String sqlStatement) throws SQLException {
+        PreparedStatement ps = conn.prepareStatement(sqlStatement);
+        PasswordSpecs pwdSpecs = pwdGenPolicy.getPasswordSpecs();
+        ps.setString(1, pwdGenPolicy.getName());
+        ps.setInt(2, pwdSpecs.getNumLowerCaseChars());
+        ps.setInt(3, pwdSpecs.getNumUpperCaseChars());
+        ps.setInt(4, pwdSpecs.getNumDigits());
+        ps.setInt(5, pwdSpecs.getNumSymbols());
+        ps.setBoolean(6, pwdGenPolicy.isDefaultPolicy());
+
+        return ps;
+    }
+
+    private PreparedStatement buildUpdatePasswordGenPolicyPreparedStatement(Connection conn, PasswordGeneratorPolicy pwdGenPolicy) throws SQLException {
+        String statement = """
+                UPDATE password_gen_policy SET password_gen_policy_name = ?, SET lower_case_chars = ?, SET upper_case_chars = ?, 
+                SET digits = ?, SET symbols = ?,  SET default_policy = ? WHERE password_policy_id = ?""";
+
+        PreparedStatement ps = buildSetOperationPreparedStatement(conn, pwdGenPolicy, statement);
+        ps.setInt(7, pwdGenPolicy.getId());
+
+        return buildSetOperationPreparedStatement(conn, pwdGenPolicy, statement);
+    }
+
+    private PreparedStatement buildDeletePasswordGenPolicyPreparedStatement(Connection conn, int id) throws SQLException {
+        PreparedStatement ps = conn.prepareStatement("DELETE FROM password_gen_policy WHERE password_gen_policy_id = ?");
         ps.setInt(1, id);
 
         return ps;
