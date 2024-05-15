f50fb681285ef256ef973b00a1102d92b920330d
Joaquin <47039871+jquinss@users.noreply.github.com>
2024-05-15 21:14:09 +0100

    created file

---
+++ src/main/java/com/jquinss/passwordmanager/controllers/PasswordPoliciesPaneController.java
@@ -0,0 +1,50 @@
+package com.jquinss.passwordmanager.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class PasswordPoliciesPaneController {
+    @FXML
+    private TableView passwordEnforcementPoliciesTableView;
+    @FXML
+    private TableColumn passwordEnforcementPolicyNameTableColumn;
+    @FXML
+    private TableColumn passwordEnforcementIsDefaultPolicyTableColumn;
+    @FXML
+    private TableView passwordGeneratorPoliciesTableView;
+    @FXML
+    private TableColumn passwordGeneratorPolicyNameTableColumn;
+    @FXML
+    private TableColumn passwordGeneratorIsDefaultPolicyTableColumn;
+
+    @FXML
+    private void addPasswordEnforcementPolicy() {
+        // TODO
+    }
+
+    @FXML
+    private void removePasswordEnforcementPolicy() {
+        // TODO
+    }
+
+    @FXML
+    private void editPasswordEnforcementPolicy() {
+        // TODO
+    }
+
+    @FXML
+    private void addPasswordGeneratorPolicy() {
+        // TODO
+    }
+
+    @FXML
+    private void removePasswordGeneratorPolicy() {
+        // TODO
+    }
+
+    @FXML
+    private void editPasswordGeneratorPolicy() {
+        // TODO
+    }
+}
