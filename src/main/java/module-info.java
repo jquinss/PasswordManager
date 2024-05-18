486051a41a2b336396684a23cf5c34a0d6fa2eb8
Joaquin <47039871+jquinss@users.noreply.github.com>
2024-05-18 14:33:51 +0100

    added opens statement

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -9,6 +9,7 @@ module com.jquinss.passwordmanager {
 
     opens com.jquinss.passwordmanager.app to javafx.fxml;
     opens com.jquinss.passwordmanager.controllers to javafx.fxml;
+    opens com.jquinss.passwordmanager.control to javafx.fxml;
 
     exports com.jquinss.passwordmanager.app;
     exports com.jquinss.passwordmanager.controllers;
