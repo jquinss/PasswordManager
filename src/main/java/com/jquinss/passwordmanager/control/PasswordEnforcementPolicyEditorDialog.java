f0fca47527cd10e7dedab6eeb9a8f0fab87c46dc
Joaquin <47039871+jquinss@users.noreply.github.com>
2024-05-19 22:13:15 +0100

    added TextFormatter to TextFields

--- src/main/java/com/jquinss/passwordmanager/control/PasswordEnforcementPolicyEditorDialog.java
+++ src/main/java/com/jquinss/passwordmanager/control/PasswordEnforcementPolicyEditorDialog.java
@@ -1,6 +1,7 @@
 package com.jquinss.passwordmanager.control;
 
 import com.jquinss.passwordmanager.data.PasswordEnforcementPolicy;
+import com.jquinss.passwordmanager.util.misc.IntRangeStringConverter;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
@@ -15,7 +16,7 @@ public class PasswordEnforcementPolicyEditorDialog extends Dialog<PasswordEnforc
     @FXML
     private TextField minLengthTextField;
     @FXML
-    private TextField minConsecCharsTextField;
+    private TextField maxConsecutiveEqualCharsTextField;
     @FXML
     private TextField minSymbolsTextField;
     @FXML
@@ -44,5 +45,19 @@ public class PasswordEnforcementPolicyEditorDialog extends Dialog<PasswordEnforc
         catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    @FXML
+    public void initialize() {
+        setTextFieldsFormatters();
+    }
+
+    private void setTextFieldsFormatters() {
+        minLengthTextField.setTextFormatter(new TextFormatter<>(new IntRangeStringConverter(1, 100), 6));
+        maxConsecutiveEqualCharsTextField.setTextFormatter(new TextFormatter<>(new IntRangeStringConverter(2, 50), 20));
+        minSymbolsTextField.setTextFormatter(new TextFormatter<>(new IntRangeStringConverter(2, 20), 2));
+        minDigitsTextField.setTextFormatter(new TextFormatter<>(new IntRangeStringConverter(2, 20), 2));
+        minUpperCaseCharsTextField.setTextFormatter(new TextFormatter<>(new IntRangeStringConverter(2, 20), 2));
+        minLowerCaseCharsTextField.setTextFormatter(new TextFormatter<>(new IntRangeStringConverter(2, 20), 2));
     }
 }
