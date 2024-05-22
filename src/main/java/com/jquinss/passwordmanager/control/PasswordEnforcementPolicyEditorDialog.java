4331d9d89e2a29725e6cb45edeb935f17c066f33
Joaquin <47039871+jquinss@users.noreply.github.com>
2024-05-22 21:39:48 +0100

    implemented class

--- src/main/java/com/jquinss/passwordmanager/control/PasswordEnforcementPolicyEditorDialog.java
+++ src/main/java/com/jquinss/passwordmanager/control/PasswordEnforcementPolicyEditorDialog.java
@@ -1,7 +1,9 @@
 package com.jquinss.passwordmanager.control;
 
 import com.jquinss.passwordmanager.data.PasswordEnforcementPolicy;
+import com.jquinss.passwordmanager.enums.PasswordPolicyEditorMode;
 import com.jquinss.passwordmanager.util.misc.IntRangeStringConverter;
+import com.jquinss.passwordmanager.util.password.PasswordStrengthCriteria;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.*;
@@ -9,6 +11,7 @@ import javafx.stage.Modality;
 import javafx.stage.Window;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class PasswordEnforcementPolicyEditorDialog extends Dialog<PasswordEnforcementPolicy> {
     @FXML
@@ -30,7 +33,13 @@ public class PasswordEnforcementPolicyEditorDialog extends Dialog<PasswordEnforc
     @FXML
     private ButtonType saveButtonType;
 
-    public PasswordEnforcementPolicyEditorDialog(Window window) {
+    private final PasswordPolicyEditorMode passwordPolicyEditorMode;
+
+    private PasswordEnforcementPolicy passwordEnforcementPolicy;
+
+    public PasswordEnforcementPolicyEditorDialog(Window window, PasswordPolicyEditorMode passwordPolicyEditorMode) {
+        this.passwordPolicyEditorMode = passwordPolicyEditorMode;
+
         try {
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(getClass().getResource("/com/jquinss/passwordmanager/fxml/PasswordEnforcementPolicyEditorPane.fxml"));
@@ -41,10 +50,53 @@ public class PasswordEnforcementPolicyEditorDialog extends Dialog<PasswordEnforc
             initModality(Modality.APPLICATION_MODAL);
             setResizable(false);
             setDialogPane(dialogPane);
+            setResultConverter(buttonType -> {
+                if (!Objects.equals(ButtonBar.ButtonData.OK_DONE, buttonType.getButtonData())) {
+                    return null;
+                }
+
+                return getPasswordEnforcementPolicy();
+            });
         }
         catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public PasswordEnforcementPolicyEditorDialog(Window window, PasswordPolicyEditorMode passwordPolicyEditorMode,
+                                                 PasswordEnforcementPolicy passwordEnforcementPolicy) {
+        this(window, passwordPolicyEditorMode);
+        this.passwordEnforcementPolicy = passwordEnforcementPolicy;
+        loadPasswordEnforcementPolicy(this.passwordEnforcementPolicy);
+    }
+
+    private PasswordEnforcementPolicy getPasswordEnforcementPolicy() {
+        switch (passwordPolicyEditorMode) {
+            case CREATE -> createPasswordEnforcementPolicy();
+            case EDIT -> modifyPasswordEnforcementPolicy();
+        }
+        return this.passwordEnforcementPolicy;
+    }
+
+    private void createPasswordEnforcementPolicy() {
+        passwordEnforcementPolicy = new PasswordEnforcementPolicy(policyNameTextField.getText(), buildPasswordStrengthCriteria());
+        passwordEnforcementPolicy.setDefaultPolicy(isDefaultPolicyCheckBox.isSelected());
+    }
+
+    private PasswordStrengthCriteria buildPasswordStrengthCriteria() {
+
+        return new PasswordStrengthCriteria.Builder().minLength(Integer.parseInt(minLengthTextField.getText()))
+                .maxConsecutiveChars(Integer.parseInt(maxConsecutiveEqualCharsTextField.getText()))
+                .minSymbols(Integer.parseInt(minSymbolsTextField.getText()))
+                .minDigits(Integer.parseInt(minDigitsTextField.getText()))
+                .minUppercaseChars(Integer.parseInt(minUpperCaseCharsTextField.getText()))
+                .minLowerCaseChars(Integer.parseInt(minLowerCaseCharsTextField.getText())).build();
+    }
+
+    private void modifyPasswordEnforcementPolicy() {
+        passwordEnforcementPolicy.setPasswordStrengthCriteria(buildPasswordStrengthCriteria());
+        passwordEnforcementPolicy.setName(policyNameTextField.getText());
+        passwordEnforcementPolicy.setDefaultPolicy(isDefaultPolicyCheckBox.isSelected());
     }
 
     @FXML
@@ -59,5 +111,17 @@ public class PasswordEnforcementPolicyEditorDialog extends Dialog<PasswordEnforc
         minDigitsTextField.setTextFormatter(new TextFormatter<>(new IntRangeStringConverter(2, 20), 2));
         minUpperCaseCharsTextField.setTextFormatter(new TextFormatter<>(new IntRangeStringConverter(2, 20), 2));
         minLowerCaseCharsTextField.setTextFormatter(new TextFormatter<>(new IntRangeStringConverter(2, 20), 2));
+    }
+
+    private void loadPasswordEnforcementPolicy(PasswordEnforcementPolicy passwordEnforcementPolicy) {
+        policyNameTextField.setText(passwordEnforcementPolicy.getName());
+        PasswordStrengthCriteria passwordStrengthCriteria = passwordEnforcementPolicy.getPasswordStrengthCriteria();
+        minLengthTextField.setText(Integer.toString(passwordStrengthCriteria.getMinLength()));
+        maxConsecutiveEqualCharsTextField.setText(Integer.toString(passwordStrengthCriteria.getMaxConsecutiveEqualChars()));
+        minSymbolsTextField.setText(Integer.toString(passwordStrengthCriteria.getMinSymbols()));
+        minDigitsTextField.setText(Integer.toString(passwordStrengthCriteria.getMinDigits()));
+        minUpperCaseCharsTextField.setText(Integer.toString(passwordStrengthCriteria.getMinUpperCaseChars()));
+        minLowerCaseCharsTextField.setText(Integer.toString(passwordStrengthCriteria.getMinLowerCaseChars()));
+        isDefaultPolicyCheckBox.setSelected(passwordEnforcementPolicy.isDefaultPolicy());
     }
 }
