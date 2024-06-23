37415e7de9598912711929861c9a58c24ce121b0
Joaquin <47039871+jquinss@users.noreply.github.com>
2024-06-23 12:08:49 +0100

    implemented class

--- src/main/java/com/jquinss/passwordmanager/controllers/PasswordGeneratorPaneController.java
+++ src/main/java/com/jquinss/passwordmanager/controllers/PasswordGeneratorPaneController.java
@@ -1,7 +1,11 @@
 package com.jquinss.passwordmanager.controllers;
 
+import com.jquinss.passwordmanager.util.password.PasswordGenerator;
+import com.jquinss.passwordmanager.util.password.PasswordSpecs;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.control.TextField;
 
 import java.net.URL;
@@ -9,30 +13,80 @@ import java.util.ResourceBundle;
 
 public class PasswordGeneratorPaneController implements Initializable {
     @FXML
-    private TextField numLowercaseCharsTextField;
+    private Spinner<Integer> numLowerCaseCharsSpinner;
 
     @FXML
-    private TextField numUppercaseCharsTextField;
+    private Spinner<Integer> numUpperCaseCharsSpinner;
 
     @FXML
-    private TextField numDigitsTextField;
+    private Spinner<Integer> numDigitsSpinner;
 
     @FXML
-    private TextField numSymbolsTetField;
+    private Spinner<Integer> numSymbolsSpinner;
 
     @FXML
-    private TextField totalNumChars;
+    private TextField totalNumCharsTextField;
 
     @FXML
-    private TextField password;
+    private TextField passwordTextField;
+
+    private PasswordGenerator passwordGenerator;
 
     @FXML
     private void generatePassword() {
         // TODO
+        passwordGenerator.setPasswordSpecs(getPasswordSpecs());
+        passwordTextField.setText(passwordGenerator.generatePassword());
+    }
+
+    private void setSpinnersListeners() {
+        setSpinnerListener(numLowerCaseCharsSpinner);
+        setSpinnerListener(numUpperCaseCharsSpinner);
+        setSpinnerListener(numDigitsSpinner);
+        setSpinnerListener(numSymbolsSpinner);
+    }
+
+    private void setSpinnerListener(Spinner<Integer> spinner) {
+        spinner.valueProperty().addListener((obs, oldValue, newValue) -> setTotalNumberOfCharsTextField());
+    }
+
+    @FXML
+    private void setTotalNumberOfCharsTextField() {
+        int total = numLowerCaseCharsSpinner.getValue() + numUpperCaseCharsSpinner.getValue() +
+                numSymbolsSpinner.getValue() + numDigitsSpinner.getValue();
+        totalNumCharsTextField.setText(Integer.toString(total));
+    }
+
+    private void initializeSpinner(Spinner<Integer> spinner) {
+        spinner.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(2, 20, 2));
+    }
+
+    private void initializeSpinners() {
+        initializeSpinner(numLowerCaseCharsSpinner);
+        initializeSpinner(numUpperCaseCharsSpinner);
+        initializeSpinner(numDigitsSpinner);
+        initializeSpinner(numSymbolsSpinner);
+    }
+
+    private void initializePasswordGenerator() {
+        passwordGenerator = new PasswordGenerator(getPasswordSpecs());
+    }
+
+    private PasswordSpecs getPasswordSpecs() {
+        return new PasswordSpecs.Builder()
+                .numLowerCaseChars(numLowerCaseCharsSpinner.getValue())
+                .numUppercaseChars(numUpperCaseCharsSpinner.getValue())
+                .numSymbols(numSymbolsSpinner.getValue())
+                .numDigits(numDigitsSpinner.getValue())
+                .build();
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         // TODO
+        initializeSpinners();
+        setSpinnersListeners();
+        setTotalNumberOfCharsTextField();
+        initializePasswordGenerator();
     }
 }
