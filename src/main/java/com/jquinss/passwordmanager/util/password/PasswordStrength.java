a3c76d10034c2acb13d2b2db60ebf486fa857d16
Joaquin <47039871+jquinss@users.noreply.github.com>
2024-07-19 21:34:36 +0100

    refactored class

--- src/main/java/com/jquinss/passwordmanager/util/password/PasswordStrength.java
+++ src/main/java/com/jquinss/passwordmanager/util/password/PasswordStrength.java
@@ -4,14 +4,12 @@ public enum PasswordStrength {
     NONE, LOW, FAIR, GOOD, EXCELLENT;
 
     public int getValue() {
-        int value = 0;
-        switch (this) {
-            case LOW -> value = 1;
-            case FAIR -> value = 2;
-            case GOOD -> value = 3;
-            case EXCELLENT -> value = 4;
-        }
-
-        return value;
+        return switch (this) {
+            case NONE -> 0;
+            case LOW -> 1;
+            case FAIR -> 2;
+            case GOOD -> 3;
+            case EXCELLENT -> 4;
+        };
     }
 }
