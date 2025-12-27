d35cda9d2752e991d8e73584859babe5ce904b56
Joaquin <47039871+jquinss@users.noreply.github.com>
2025-12-27 13:55:31 +0000

    refactored class to use DataSourceType enum and url

--- src/main/java/com/jquinss/passwordmanager/factories/DataSourceFactory.java
+++ src/main/java/com/jquinss/passwordmanager/factories/DataSourceFactory.java
@@ -1,13 +1,19 @@
 package com.jquinss.passwordmanager.factories;
 
 import javax.sql.DataSource;
+
+import com.jquinss.passwordmanager.enums.DataSourceType;
 import org.sqlite.SQLiteDataSource;
 
 public class DataSourceFactory {
-    public static DataSource getDataSource(String type) {
-        return switch (type) {
-            case "SQLITE" -> new SQLiteDataSource();
-            default -> throw new IllegalArgumentException();
-        };
+    public static DataSource getDataSource(DataSourceType type, String URL) {
+        if (type == DataSourceType.SQLITE) {
+            SQLiteDataSource sqLiteDataSource = new SQLiteDataSource();
+            sqLiteDataSource.setUrl(URL);
+            return sqLiteDataSource;
+        }
+        else {
+            throw new IllegalArgumentException();
+        }
     }
 }
