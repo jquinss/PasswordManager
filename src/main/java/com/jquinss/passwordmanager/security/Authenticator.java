cef5d0e581729a8eddeffd02da5010e3f167ba46
Joaquin <47039871+jquinss@users.noreply.github.com>
2025-12-31 18:50:27 +0000

    removed unused imports

--- src/main/java/com/jquinss/passwordmanager/security/Authenticator.java
+++ src/main/java/com/jquinss/passwordmanager/security/Authenticator.java
@@ -1,13 +1,11 @@
 package com.jquinss.passwordmanager.security;
 
 import com.jquinss.passwordmanager.data.UserProfile;
-import com.jquinss.passwordmanager.managers.DatabaseManager;
 import com.jquinss.passwordmanager.managers.SettingsManager;
 import com.jquinss.passwordmanager.util.misc.CryptoUtils;
 
 import java.sql.SQLException;
 import java.util.Arrays;
-import java.util.Optional;
 
 public class Authenticator {
     public boolean authenticate(UserProfile userProfile, String password) throws SQLException {
