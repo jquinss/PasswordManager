4adda3e44406908131a2f46b340fd0763b8e9a12
Joaquin <47039871+jquinss@users.noreply.github.com>
2026-05-15 20:32:26 +0100

    removed unused methods

--- src/main/java/com/jquinss/passwordmanager/managers/SettingsManager.java
+++ src/main/java/com/jquinss/passwordmanager/managers/SettingsManager.java
@@ -9,16 +9,10 @@ public class SettingsManager {
     private static final String DATA_FOLDER_NAME = "data";
     private static final String DB_NAME = "database.db";
     private static final SettingsManager settingsManager = new SettingsManager();
-    private final int passwordHashLengthInBytes = 32;
-    private final int saltLengthInBytes = 64;
-    private final String keyPairAlgorithm = "RSA";
-    private final String symmetricEncryptionAlgorithm = "AES/CBC/PKCS5Padding";
-    private final int ivParameterSpecLengthInBytes = 16;
-    private final int keyPairLengthInBits = 2048;
 
-    private static PasswordStrength minPasswordStrength = PasswordStrength.EXCELLENT;
-
-    private SettingsManager() {}
+    private SettingsManager() {
+        System.out.println("Inside SettingsManager constructor");
+    }
 
     public static SettingsManager getInstance() {
         return settingsManager;
@@ -30,33 +24,5 @@ public class SettingsManager {
 
     public String getDatabaseDir() {
         return Path.of(OSChecker.getOSDataDirectory(), APP_FOLDER_NAME, DATA_FOLDER_NAME).toString();
-    }
-
-    public int getPasswordHashLengthInBytes() {
-        return passwordHashLengthInBytes;
-    }
-
-    public int getSaltLengthInBytes() {
-        return saltLengthInBytes;
-    }
-
-    public PasswordStrength getMinPasswordStrength() {
-        return minPasswordStrength;
-    }
-
-    public String getKeyPairAlgorithm() {
-        return keyPairAlgorithm;
-    }
-
-    public String getSymmetricEncryptionAlgorithm() {
-        return symmetricEncryptionAlgorithm;
-    }
-
-    public int getIvParameterSpecLengthInBytes() {
-        return ivParameterSpecLengthInBytes;
-    }
-
-    public int getKeyPairLengthInBits() {
-        return keyPairLengthInBits;
     }
 }
